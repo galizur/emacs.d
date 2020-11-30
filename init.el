c76373fabe0683463b3d372efaafb77b6b37c3f2
Karolos Triantafyllou <karolos.triantafyllou@gmail.com>
2020-11-30 16:51:33 +0200

    Changes to layout and customization

--- init.el
+++ init.el
@@ -1,11 +1,9 @@
-(setq gc-cons-threshold (* 50 1000 1000))
-
 (require 'package)
 (package-initialize)
+
+(require 'org)
+(require 'ob-tangle)
 
 (if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
     (load-file (expand-file-name "config.el" user-emacs-directory))
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
-
-;; Make gc pauses faster by decreasing the threshold.
-(setq gc-cons-threshold (* 10 1000 1000))
