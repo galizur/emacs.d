8dc5679ba9ed502dca8e80481fc96b576663c03c
Karolos Triantafyllou <karolos.triantafyllou@gmail.com>
2021-09-30 12:00:53 +0300

    Redesign of emacs

--- init.el
+++ init.el
@@ -1,9 +1,10 @@
+(setq gc-cons-threshold (* 100 1024 1024))
+
 (require 'package)
 (package-initialize)
-
-(require 'org)
-(require 'ob-tangle)
 
 (if (file-exists-p (expand-file-name "config.el" user-emacs-directory))
     (load-file (expand-file-name "config.el" user-emacs-directory))
   (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
+
+(setq gc-cons-threshold (* 10 1000 1000))
